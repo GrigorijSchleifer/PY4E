e04d492198736d49a2897689b576abf43e6d1a6b
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-09-11 20:49:53 +0200

    functions in a list do not need parenthesis

--- L_1.py
+++ L_1.py
@@ -8,3 +8,19 @@ while True:
     if inp == 'done':
         print(inp)
         break
+
+
+# codewars first method for chaining
+def fct_one(x): 
+    return x * 10
+def fct_two(x): 
+    return x * 10
+
+functions_list = [fct_one, fct_two]
+
+def chain(value, functions: list) -> int:
+    for i in functions:
+        value = i(value)
+    return print(value)
+
+chain(100, functions_list)
