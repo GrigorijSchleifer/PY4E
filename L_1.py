ad102f22fc942036f4faed3f7c939320c422865e
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-09-11 20:10:01 +0200

    continue in a while loop

--- L_1.py
+++ L_1.py
@@ -1,5 +1,10 @@
+
+# while loop with continue
 while True:
     inp = input("> ")
+    if inp == '#':
+        print(f'first sign of the input is {inp[0]}')
+        continue 
     if inp == 'done':
-        print('Good by')
+        print(inp)
         break
