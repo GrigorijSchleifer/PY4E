09573540ba16d460082688280db7b0407e594056
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-09-11 20:04:33 +0200

    break in a while loop

--- L_1.py
+++ L_1.py
@@ -1,10 +1,5 @@
-condition = True
-
-while condition:
-    try:
-        inp = input("type in a number")
-        inp = int(inp)
-        condition = False 
-    except:
-        print("Should be an integer")
-
+while True:
+    inp = input("> ")
+    if inp == 'done':
+        print('Good by')
+        break
