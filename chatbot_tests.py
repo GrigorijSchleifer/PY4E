c19ee6be12674446694577a39bc02403566fbbc0
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-09-20 22:16:36 +0200

    Some modifications of read files

--- chatbot_tests.py
+++ chatbot_tests.py
@@ -1,3 +1,6 @@
+import readline
+
+
 with open('chatbot.txt') as chb:
     chb_chars = chb.readlines()
 
@@ -6,7 +9,24 @@ print(chb_chars[:10])
 line_number = 0
 
 for line in chb_chars:
+    # strip new line character from the end of the line
+    line = line.rstrip()
     if line.startswith("Warum "):
         print(line)
 
+# a different way to thing about the loop
+for line in chb_chars:
+    line = line.rstrip()
+    if not line.startswith("Warum"):
+        continue
+    else:
+        print(line)
+
+# printing only questions
+for line in chb_chars:
+    line = line.rstrip()
+    if not "?" in line:
+        continue
+    else:
+        print(line)
 
