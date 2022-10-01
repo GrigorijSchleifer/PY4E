e46918dd0742feebf1d430b3633f4f3af4cfd064
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-10-01 08:54:50 +0200

    ...

--- average_while_loop.py
+++ average_while_loop.py
@@ -2,16 +2,19 @@ def collect_input():
     """
     Asking user for a number, if input is not a number,
     catching eception and returning to the input prompt
+
+    Args: no arguments
+    Return: computated average of values in the list
     """
+    lst = list()
     try:
         while True:
             input_variable = input("Your input: \n\n Type \"done\" for exit \n")
             if input_variable == 'done': break
+            lst.append(int(input_variable))
     except ValueError:
         print("Only integers are allowed! ")
         collect_input()
+    return print(f"We have the list: {lst} with the average {sum(lst)/len(lst)}")
 
-try:
-    collect_input()
-except ValueError:
-    print('Input has to be an integer')
+collect_input()
