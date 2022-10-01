821a0d6a270a793c8bf3fb226fdd2229c606d43d
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-10-01 23:09:44 +0200

    endswith("?") nice!

--- Oktober/add_QA_at_beginning.py
+++ Oktober/add_QA_at_beginning.py
@@ -6,6 +6,10 @@ try:
     # r+: read and write mode
     with open("chatbot.txt", "r+") as f:
         for line in f:
-            print(line) 
+            line = line.rstrip()
+            if line.endswith("?"):
+                print(f'Q: {line}') 
+            else:
+                print(f'A: {line}')
 except FileNotFoundError:
     print('No file found')
