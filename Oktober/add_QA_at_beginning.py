cb5a1399c7f1a82c43f8e62057f047631c90c5b4
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-10-01 21:52:08 +0200

    I still don't understend the open method

--- Oktober/add_QA_at_beginning.py
+++ Oktober/add_QA_at_beginning.py
@@ -1,14 +1,19 @@
+"""
+Reading in the Q/As, and adding Q: or A: depending on line number
+Odd line numbers:       "Q: " is appendet
+Non odd lien nimbers:   "A: " is appendet
+"""
 try:
-    with open("chatbot.txt") as file_object:
-        file_object = file_object.readlines()
+    # r+: read and write mode
+    with open("chatbot.txt", "r+") as file:
+        file_object = file.readlines()
+        print(type(file_object))
+
+        for line_index in range(len(file_object)):
+            line = file_object[line_index].rstrip()
+            if (line_index + 1) % 2 != 0:
+                print("Q: " + line, file=file_object)
+            else:
+                print("A: " + line, file=file_object)
 except FileNotFoundError:
     print('No file found')
-
-for line_index in range(len(file_object)):
-    line = file_object[line_index].rstrip()
-    if (line_index + 1) % 2 != 0:
-        print("Q: " + line)
-    else:
-        print("A: " + line)
-        
-
