5b1f637991241566855855aed7eebca0aaf11460
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-12-11 20:57:46 +0100

    PfE: try except block

--- Exercises/ex06.py
+++ Exercises/ex06.py
@@ -1,9 +1,12 @@
+extra_hours_ratio = 1.5
+
 while True:
     try:
         hours = int(input("How many hour do you work? \n"))
         rate_per_hour = int(input("What is you hourly pay? \n"))
+        extra_hours = int(input("How many extra hours did you work? \n"))
         break
     except ValueError:
         print('Only integers are alowed')
 
-print(f"You will earn {hours} * {rate_per_hour} dollars for your shift")
+print(f"You will earn {(hours * rate_per_hour) + (extra_hours * extra_hours_ratio)} dollars for your shift")
