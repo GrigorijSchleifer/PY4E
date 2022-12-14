4aa41a30b098d5b143a1e43b4a9460dc772231cc
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-12-14 21:16:38 +0100

    PfE: ch3_ex3 solved, but without match case

--- Exercises/December/ch3_ex03.py
+++ Exercises/December/ch3_ex03.py
@@ -7,18 +7,17 @@
 #   >= 0.7    C
 #   >= 0.6    D
 #   < 0.6     F
+def score_grader(grade = None):
+    while True:
+        try:
+            score = float(input("What was your score? \n"))
+            if score < 0 or score > 1:
+                print("Score has to be between 0 and 1 \n")
+                continue
+            break
+        except ValueError:
+            print("Only integers are alowed \n")
 
-while True:
-    try:
-        score = float(input("What was your score? \n"))
-        if score < 0 or score > 1:
-            print("Score has to be between 0 and 1 \n")
-            continue
-        break
-    except ValueError:
-        print("Only integers are alowed \n")
-
-def score_grader(score, grade = None):
     if score >= 0.9:
         grade = "A" 
     elif score >= 0.8:
@@ -31,4 +30,4 @@ def score_grader(score, grade = None):
         grade = "F"
     return grade
 
-print(f'Your score is {score_grader(score, grade=None)}')
+int(f'Your score is {score_grader(grade=None)}')
