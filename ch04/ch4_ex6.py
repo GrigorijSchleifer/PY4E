261e7f4a6db379c295731ba462e45271e6c75cec
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2022-12-20 20:24:32 +0100

    PfE: Carries suggestions implemented for ch4ex6

--- ch04/ch4_ex6.py
+++ ch04/ch4_ex6.py
@@ -22,7 +22,7 @@ def workhours_pay() -> tuple:
 # how many hours over normal shift
 def over_time(hours: int) -> int:
     regular_shift = 8
-    return regular_shift - hours
+    return abs(regular_shift - hours)
 
 # return time was incorrect 
 def computepay(hours: int, rate: int, over_time: int) -> int:
@@ -32,26 +32,13 @@ def computepay(hours: int, rate: int, over_time: int) -> int:
         rate {int}: rate per hour
         over_time {int}: takes the over_time function to return hours worked over time
     Return:
-        
+         
     """
-    # amazing!!! all without brackets and ""
-    # no printed object should be returned
     return f"{((hours - over_time) * rate) + (over_time * (rate * 1.5))}"
 
-# assigning (unpacking) hour and rate
-# no brackets needed!!! 
 hours, rate = workhours_pay()
+print(computepay(hours, rate, over_time(hours)))
 
-computepay(hours, rate, over_time())
 
 
-# In terms of the workhours_pay() function:
-# - you should be able to return hours, pay without the need for the brackets. They will be a tuple by default
-# - the docstring for return is incorrect. you are returning a tuple. it should be `Returns: tuple(int, int): hours, pay per hour 
-# with computerpay, it is bad practice to include print statements in a function. Functions should return something - in this case, i would just return the string that you have constructed.
-# the reason we don't use print statements in functions generally is that once it has run the data is lost, and to get it back, you need to run again, whereas if you run it and a varaible with the outputs, it can be printed many times, or saved or something else
-# i don't think you need abs for your calculation of overtime. This just gets an absolyte numbers (for example, abs(20) and abs(-20) both return 20)
-# i think here you were trying to remove the minus if this came into play. This is a way of doing it but I personally would just have done this as a function
-# my personal preference though and not necessarily a criticism
-# finally, functions should be defined at the top and called at the bottom.
-# other than that, all great! well done!!
+
