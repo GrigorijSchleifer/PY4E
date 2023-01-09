682b4cdfd69e809d104a6efe1894f0dd768ad079
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-09 21:35:39 +0100

    PfE: Book pdf added and new exersice started

---
+++ ch06/ch06ex03.py
@@ -0,0 +1,4 @@
+# Encapsulate this code in a function named count, and generalize 
+# it so that it accepts the string and the letter as arguments.
+
+# don't really understand what this exercise wants me to accomplish
