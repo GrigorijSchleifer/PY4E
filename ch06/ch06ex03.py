363d766af49e8a9c7ffab4ee4540fd6d337628f0
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-15 14:11:14 +0100

    PfE: 0603 return statement should be outside of a loop

--- ch06/ch06ex03.py
+++ ch06/ch06ex03.py
@@ -1,13 +1,31 @@
 # Encapsulate this code in a function named count, and generalize 
 # it so that it accepts the string and the letter as arguments.
 
+# the count of characters to look for will be stored here
 count = 0
+
+def check_for_done_input(inpt):
+    """ 
+    Checking if the user wants to quit the loop and enters 'done'
+
+    Args:       string from the user
+    
+    Return:     Boolean, if the input was 'done', the input of
+                ask_for_word and ask_for_chat will be stoped
+    """
+    done_or_not = False
+
+    if inpt == 'done':
+        done_or_not = True
+    else:
+        done_or_not = False
+    return done_or_not 
 
 def ask_for_word():
     while True:
         try:
-            inp = str(input('What word should we scan for a string?'))
-            if inp == 'done':
+            inp = str(input('What word should we scan for a string? \n'))
+            if check_for_done_input(inp) == False:
                 break
         except ValueError:
             print('This is not a string')
@@ -16,16 +34,14 @@ def ask_for_word():
 def ask_for_char():
     while True:
         try:
-            count_char = str(input('What character should we count?'))
-            if count_char == 'done':
+            count_char = str(input('What character should we count? \n'))
+            if check_for_done_input(count_char) == False:
                 break
         except ValueError:
             print('Not a character')
-        return count_char
+    return count_char
 
 def count_char(word, chr, count):
-    print(word)
-    print(chr)
     for i in word:
         if i == chr:
             count += 1
