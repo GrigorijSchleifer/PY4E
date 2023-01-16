deffcf02559e45cda794e8bc255703ec07319021
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-16 20:22:30 +0100

    PfE: not much happening

--- ch06/ch06ex04.py
+++ ch06/ch06ex04.py
@@ -6,4 +6,16 @@
 # Wriii an invocation that counts the number of times the letter a occurs
 # in “banana”
 
+def check_if_done(done):
+    return True if done == 'done' else False
+
+def count():
+    words_for_counting = []
+    while True:
+        try:
+            words_for_counting.append(str(input('What word do you want \n')))
+            if check_if_done(words_for_counting[-1]) == True:
+                break
+        except ValueError:
+            print('Only strings')
 
