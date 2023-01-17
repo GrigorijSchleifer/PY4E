93656cbdd9c8920ad6d3813c5717b6168ccc4b4f
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-17 20:28:40 +0100

    PfE: Working on next exercise to import  a text file and use its lines for the ch06ex05 exercise

--- ch06/ch06ex05.py
+++ ch06/ch06ex05.py
@@ -4,6 +4,26 @@
 # Use find and string slicing to extract the portion of the string after the
 # colon character and then use the float function to convert the extracted
 
-str = 'X-DSPAM-Confidence:0.8475'
-str.find(:)
+
+"""
+Goal is to open a file with some text data and filter it by ':'
+"""
+
+with open('file_to_write.txt', 'w') as file_to_write:
+    for line in lines:
+        prefix = 'Q: ' if line.endswith('?') else 'A: '
+        file_to_write.write(prefix + line + '\n')
+
+
+try:
+    with open('text_to_modify.txt', 'r') as file_to_read:
+        lines = file_to_read.read().split('\n')
+except FileNotFoundError:
+    print("No such file found")
+
+with open('file_to_write.txt', 'w') as file_to_write:
+    for line in lines:
+        
+        prefix = 'Q ' if 
+
 
