e199ee5b2a4d0555ae6d0152d9969476037c5a5f
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-20 09:07:28 +0100

    test for mobile workflow

--- ch06/ch06ex05.py
+++ ch06/ch06ex05.py
@@ -2,7 +2,7 @@
 # str = 'X-DSPAM-Confidence:0.8475'
 # Use find and string slicing to extract the portion of the string after the
 # Use find and string slicing to extract the portion of the string after the
-# colon character and then use the float function to convert the extracted
+# colon character and then use the float function to convert the extracted hey
 
 
 """
