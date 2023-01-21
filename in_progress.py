f5112eb350c9f061b2a1c1714bc0f5c8c6efe0c8
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-21 16:25:06 +0100

    data folder created to store the data for string modification

--- in_progress.py
+++ in_progress.py
@@ -10,11 +10,12 @@ Goal is to open a file with some text data and filter it by ':'
 """
 
 try:
-    with open('text_to_modify.txt', 'r') as file_to_read:
+    with open('data/text_to_modify.txt', 'r') as file_to_read:
         lines = file_to_read.read().split('\n')
+        print(lines)
 except FileNotFoundError:
     print("No such file found")
 
-with open('file_to_write.txt', 'w') as file_to_write:
+with open('data/file_to_write.txt', 'w') as file_to_write:
     for line in range(0, len(lines)):
-        print(f"For {line.split(':')[0]} the rounded integer is {lines.split(':')[1]}")
+            print(f"For {line.split(':')[0]} the Type of the second input is: {type(line.split(':')[1])}")
