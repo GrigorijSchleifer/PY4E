bf12d859daa55f9632332654add99be415f93e9a
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:07:10 +0100

    re module implemented

--- in_progress.py
+++ in_progress.py
@@ -3,6 +3,6 @@ import re
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    only_author = f_contents.findall(r'[Author:]\w+')
+    only_author = re.findall(r'[Author:]\w+')
     for itm in only_author:
         print(itm, '\n')
