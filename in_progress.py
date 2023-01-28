b2114f401b709898f2821351a6426fde7d936038
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:45:07 +0100

    ...

--- in_progress.py
+++ in_progress.py
@@ -3,8 +3,6 @@ import re
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    rgx = re.compile(r'[\Author:]')
+    pattern = "Author:"
 
-    for itm in f_contents:
-        if re.search(rgx, itm):
-            print(itm)
+    print(re.search(pattern, itm))
