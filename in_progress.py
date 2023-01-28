6ff0a52ff89cca3cd9a360e0ef3695297bb6cf41
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:33:00 +0100

    re.compile and re.search ... will need to look this up

--- in_progress.py
+++ in_progress.py
@@ -3,6 +3,8 @@ import re
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    only_author = re.findall(r'[Author:]\w+', f_contents)
-    for itm in only_author:
-        print(itm, '\n')
+    rgx = re.compile(r'[Author:]\+')
+
+    for itm in f_contents:
+        if re.search(rgx, itm)
+        print(itm)
