40f889609803303f0ec1cb780b2e07f150ac9228
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:46:12 +0100

    HelloWorld

--- in_progress.py
+++ in_progress.py
@@ -2,7 +2,7 @@ import re
 
 # this was weired
 with open('data/mbox_short.txt') as f:
-    f_contents = f.readlines()
+    f_contents = f.read()
     pattern = "Author:"
 
     print(re.search(pattern, f_contents))
