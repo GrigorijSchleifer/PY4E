6513afa911be0f6877c0a94a95150327b65f1164
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:40:56 +0100

    regex adjusted

--- in_progress.py
+++ in_progress.py
@@ -3,7 +3,7 @@ import re
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    rgx = re.compile(r'[Author:]\+')
+    rgx = re.compile(r'[\Author:]')
 
     for itm in f_contents:
         if re.search(rgx, itm):
