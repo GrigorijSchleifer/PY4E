760f5a4aac842d39908d365a6745429178b18ba4
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 22:08:04 +0100

    all this commiting is too much

--- in_progress.py
+++ in_progress.py
@@ -3,6 +3,6 @@ import re
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    only_author = re.findall(r'[Author:]\w+')
+    only_author = re.findall(r'[Author:]\w+', f_contents)
     for itm in only_author:
         print(itm, '\n')
