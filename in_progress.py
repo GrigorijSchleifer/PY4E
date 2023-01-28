56b0946f1a00847237dd78fc1b51257e741f4210
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-28 21:57:56 +0100

    first regex to find all lines with "Author:"

--- in_progress.py
+++ in_progress.py
@@ -1,5 +1,8 @@
+import re
+
 # this was weired
 with open('data/mbox_short.txt') as f:
     f_contents = f.readlines()
-    for itm in f_contents:
+    only_author = f_contents.findall(r'[Author:]\w+')
+    for itm in only_author:
         print(itm, '\n')
