6079c13867dc69636cc58ea55ab968c80e8182ca
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-30 23:02:07 +0100

    PfE: prompt for next attempt to enter a new file name

--- in_progress.py
+++ in_progress.py
@@ -3,7 +3,8 @@ import re
 def open_file(fl_name: str) -> str:
     # this was weired
     with open(fl_name) as f:
-        f_contents = f.readlines()
+        fl_name = input("Tell me the name of the file: ")
+        f_contents = fl_name.readlines()
         cnt = 0
         for ln in f_contents:
             ln = ln.rstrip()
@@ -11,10 +12,12 @@ def open_file(fl_name: str) -> str:
                 if not ln.startswith('From:'): continue
                 cnt = cnt + 1
     return f'There are {cnt} lines that start with \'From:\''
-    
 
+def try_again():
+    yes_or_no = input('Do you want another shot?')
+    if yes_or_no.lower() == 'yes':
+        open_file()
 try:
-    fl_name = input("Tell me the name of the file: ")
     print(open_file(fl_name))
 except:
     print("File cannot be found")
