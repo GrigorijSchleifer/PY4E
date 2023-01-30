5cf95139eb2acb28a60c4f2aa5d3c3d9acaccb0a
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-30 19:58:05 +0100

    see if there are clashes

--- in_progress.py
+++ in_progress.py
@@ -2,15 +2,11 @@ import re
 
 # this was weired
 with open('data/mbox_short.txt') as f:
-<<<<<<< HEAD
     f_contents = f.readlines()
-    cnt = 0
     for ln in f_contents:
-        count =+ 1 
-        print(count) 
-=======
-    f_contents = f.read()
-    pattern = "Author:"
-
-    print(re.search(pattern, f_contents))
->>>>>>> 40f889609803303f0ec1cb780b2e07f150ac9228
+        ln = ln.rstrip()
+        if ln.startswith('From:'):
+            # will skip lines without "From:"
+            if not ln.startswith('From:'):
+                continue
+            print(ln)
