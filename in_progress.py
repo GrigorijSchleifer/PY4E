2ae8dd9803ba73fc2fa82bd75babeb520d4d502c
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-01-30 22:43:22 +0100

    PfE: File input and catching the error

--- in_progress.py
+++ in_progress.py
@@ -1,12 +1,22 @@
 import re
 
-# this was weired
-with open('data/mbox_short.txt') as f:
-    f_contents = f.readlines()
-    for ln in f_contents:
-        ln = ln.rstrip()
-        if ln.startswith('From:'):
-            # will skip lines without "From:"
-            if not ln.startswith('From:'):
-                continue
-            print(ln)
+def open_file(fl_name: str) -> str:
+    # this was weired
+    with open(fl_name) as f:
+        f_contents = f.readlines()
+        cnt = 0
+        for ln in f_contents:
+            ln = ln.rstrip()
+            if ln.startswith('From:'):
+                if not ln.startswith('From:'): continue
+                cnt = cnt + 1
+    return f'There are {cnt} lines that start with \'From:\''
+    
+
+try:
+    fl_name = input("Tell me the name of the file: ")
+    print(open_file(fl_name))
+except:
+    print("File cannot be found")
+    exit()
+
