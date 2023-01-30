e1a291d6a2e186e6ec33c45df36efb22e26b3705
root <root@localhost.localdomain>
2023-01-30 17:46:32 +0100

    KopfKörper commit

--- in_progress.py
+++ in_progress.py
@@ -1,16 +1,9 @@
 import re
 
-# this was weired
 with open('data/mbox_short.txt') as f:
-<<<<<<< HEAD
     f_contents = f.readlines()
-    cnt = 0
     for ln in f_contents:
-        count =+ 1 
-        print(count) 
-=======
-    f_contents = f.read()
-    pattern = "Author:"
+        ln = ln.rstrip()
+        if ln.startswith("From:"):
+            print(ln)
 
-    print(re.search(pattern, f_contents))
->>>>>>> 40f889609803303f0ec1cb780b2e07f150ac9228
