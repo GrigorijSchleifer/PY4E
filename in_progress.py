ba769f94fb00b15b03481e78f2a80278cae17703
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-03-14 19:32:45 +0100

    PFE: Difficult!

--- in_progress.py
+++ in_progress.py
@@ -6,9 +6,33 @@
 # create a heuristic for defining the second argument for the open() method
 # make an infinite loop to ask for the second argument of open() and quiting user wants to
 # guard the ln.split() line in case the file is opened as binary not pure text.
+def return_mode(argument):
+    argument = {
+
+    }
+
 def argument_for_open():
-    try
-with open('data/mbox_short.txt') as fhand:
+    try:
+        print("""Choose the mode for the open method:
+        1: read
+        2: write
+        3: binary
+        """)
+        inp_mode = int(input("Your choice: \n"))
+        # limit the input to number between 1 - 3
+        if inp_mode in range(1, 3):
+            return 
+        else:
+            print("Pleas choose the option 1-2")
+    except ValueError:
+        print("Only integers allowed")
+        argument_for_open()
+    return inp_mode
+
+
+
+# argument_for_open() will prompt user to define the mode for the open method
+with open('data/mbox_short.txt', str(argument_for_open())) as fhand:
     for ln in fhand:
         words_splitonly = ln.split()
         if len(words_splitonly) == 0: continue
