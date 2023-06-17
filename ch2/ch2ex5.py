8725942c96e7753151a43b5fc33176567739ec86
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-06-17 23:26:10 +0200

    Code improvements for ex5 in chapter 2

--- ch2/ch2ex5.py
+++ ch2/ch2ex5.py
@@ -1,4 +1,10 @@
-def ask_for_payhoury():
+def ask_for_payhoury() -> int:
+    """Asking for hour worked and pay per our
+    Ags:
+        No arguments
+    Return:
+        Integers for hours worked and pay per hour are returned
+    """
     while True:
         try:
             hours = int(input("How many hours did you work today? \n"))
@@ -6,15 +12,27 @@ def ask_for_payhoury():
             break
         except ValueError:
             print("Only integers please")
+    return hours, pay
 
-# if worker does more that 40 hours
+# assign hours worked and pay per hour  
+hours, pay = ask_for_payhoury()
+
 # every extra hour above 40 will be paid with a 1.5 coefficient
 extra_hours_coeff = 1.5
 
-if hours > 40: 
-    pay_extra_hours = (40 * pay) + ((hours - 40) * pay) * extra_hours_coeff
-    print(f"This will bring you {pay_extra_hours} per hour")
-else:
-    print(f"Your payroll will be {hours * pay}")
+def calculate_pay(hours: int, pay: int, coef: float) -> None:
+    """Calculation amd printing pay per hour
+    Args:
+        hours: how many hours were worked
+        pay: pay per hour
+        coeff: every extra hour above 40 will be paid with a 1.5 coefficient 
+    Return:
+        String: Calculated pay
+    """
+    if hours > 40: 
+        pay_extra_hours = (40 * pay) + ((hours - 40) * pay) * coef
+        print(f"This will bring you {pay_extra_hours} per hour")
+    else:
+        print(f"Your payroll will be {hours * pay}")
 
-print("")
+calculate_pay(hours, pay, extra_hours_coeff)
