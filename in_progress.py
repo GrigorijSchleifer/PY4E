82f82ac6cfa38dfbfdfe957c1ab70a39dba7eb27
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2023-10-19 08:29:01 +0200

    try harder

--- in_progress.py
+++ in_progress.py
@@ -1,19 +1,49 @@
 # gamma calculator
-
 from typing import List, Any
 
-# empty dictonary where catecholamins and infusion rate should be stored
-ctlms = {}
+dct_ctlms = {}
+
+def ask_catecholamins(dct: dict) -> str:
+    """ Asking for catecholamins and flowrates to fill the dct_ctlms  
+
+    Returns:
+        str: _description_
+    """  
+    while True:
+        try:
+            ctlm = input("What catecholamins are running? \n")
+        except ValueError:
+            print("Only characters are allowed")
+        
+        # assign None value to a catecholamine
+        # None will be raplaces in ask_rate()
+        dct[ctlm] = None
+
+        # exiting the loop
+        if ctlm.lower() == 'exit':
+            break
+    
+    return dct 
+
+def ask_rate() -> str:
+    """_summary_
+
+    Returns:
+        str: _description_
+    """    
+    return None
 
 def dspl_ctlms() -> str:
-    """Function askinng user for the running catecholamins
+    """Function asking user for the running catecholamins
     
     Args:
-        no arguments, user will provide them into the 
+        no arguments, user will provide them into the ctlms dictionary
+
     Return"
+        ctlms (str): dictionary filled with catecholamins and infusion rate
 
     """
+    return None
 
-
-
-
+# use ask_catecholamins to fill the dct_ctlms dict with catecholamins and flowrates
+print(ask_catecholamins(dct_ctlms))
