63afd4511ce95dcf3708bc59b5d36908823abb6a
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-01-22 21:37:59 +0100

    Tired but not giving up!

--- in_progress.py
+++ in_progress.py
@@ -1,5 +1,5 @@
-# Exercise 3: Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
-scores_for_grades = {
+# Exercise 3: Write a program to prompt for a score_range between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
+score_ranges_for_grades = {
     (0.0, 0.5): "F",
     (0.51, 0.6): "E",
     (0.61, 0.7): "D", 
@@ -8,9 +8,9 @@ scores_for_grades = {
     (0.91, 1.0): "A"
 }
 
-def ask_score() -> float:
-    """Write a program to prompt for a score betwween 0.0 and 1.0. If the score is out of range print an error message. 
-    If the score is between 0.0 and 1.0 print a grade using the following table. 
+def ask_score_range() -> float:
+    """Write a program to prompt for a score_range betwween 0.0 and 1.0. If the score is out of range print an error message. 
+    If the score_range is between 0.0 and 1.0 print a grade using the following table. 
 
     Returns:
         int: _description_
@@ -19,31 +19,31 @@ def ask_score() -> float:
         try:
             score = float(input("What was your score? \n"))
             if score < 0 or score > 1:
-                print("Score has to be between 0 and 1 \n")
+                print("score_range has to be between 0 and 1 \n")
                 continue
-            print(f"Score is {score}")
+            print(f"score_range is {score}")
             break
         except ValueError:
             print("Only integers are alowed \n")
-    return score 
+    return score
 
-def find_grade(score: float) -> str:
-    """Take the score from ask_score and get the grade associated with this score
+def find_grade(score_range: float) -> str:
+    """Take the score_range from ask_score and get the grade associated with this score
 
     Args:
-        score (float): the score from the ask_score method
+        score_range (float): the score from the ask_score method
 
     Returns:
         str: 
     """
-    for range, grade in scores_for_grades.items():
-        # if the score is inside the range
-        if range[0] <= score <= range[1]:
+    for range, grade in score_ranges_for_grades.items():
+        # if the score_range is inside the range
+        if range[0] <= score_range <= range[1]:
             print(f"{range[0]} and {range[1]}\n")
             return grade
 
 def print_grade(grade: str) -> str:
-    """Takes the grade (ask_score -> find_grade) and prints it 
+    """Takes the grade (ask_score_range -> find_grade) and prints it 
 
     Args:
         grade (chr): the grade from the find_grade method 
@@ -54,7 +54,9 @@ def print_grade(grade: str) -> str:
     print(f"Your grade is: {grade}")
 
 def main():
-    print_grade(find_grade(ask_score()))
+    user_score_range = ask_score()
+    grade = find_grade(user_score_range)
+    print_grade(grade)
 
 if __name__ == "__main__":
     main()
