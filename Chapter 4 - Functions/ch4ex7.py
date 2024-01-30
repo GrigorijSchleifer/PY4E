f57a32fd7983332ef119f69e166b3912fdb41f6f
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-01-30 19:48:24 +0100

    some munging

--- Chapter 4 - Functions/ch4ex7.py
+++ Chapter 4 - Functions/ch4ex7.py
@@ -8,6 +8,41 @@
 #   >= 0.6  D
 #   < 0.6   F
 
+SCORE_GRADES = {
+    (1.0, 0.9): 'A',
+    (0.89, 0.8): 'B',
+    (0.79, 0.7): 'C', 
+    (0.69, 0.6): 'D',
+    (0.59, 0.0): 'F'
+}
+
+def ask_for_score() -> int:
+    while True:
+        try:
+            score = int(input("Your score: \n"))
+            break
+        except ValueError:
+            print("Only integers allowed")
+    return score
+
+
+def check_grade(score_user: int) -> str:
+    """Takes users score and matches it to the drade from SCORE_GRADES
+    If users score is in the range of  
+
+    Args:
+        score_user (int): _description_
+
+    Returns:
+        str: _description_
+    """
+    for score, grade in SCORE_GRADES.items():
+        if score[0] <= score_user <= score[1]:
+            return grade
+        break
+
+
+
 def score_grader(grade = None):
     while True:
         try:
